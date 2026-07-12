479701f12ebb9b185cf64c08ffec61ee7c74a1ec
SRI SAI YANDRA <147787845+srisaigi@users.noreply.github.com>
2026-07-12 12:53:12 +0530

    Add: reverse-integer - submission-1

---
+++ Data Structures & Algorithms/reverse-integer/submission-1.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int reverse(int x) {
+        int a = Math.abs(x); // Magic starts here...for implementational......
+        int sum =0;
+        while(a > 0){
+            int rem = a % 10;
+            if(sum > (Integer.MAX_VALUE)/10){
+                return 0;
+            }
+            sum = sum * 10 + rem;
+            a = a / 10;
+        }
+        return (x > 0) ? sum : -sum;
+    }
+}
